1af52c6f81a75975885eaa61cccb93eb329f8762
andrei.amorado <andrei.amorado@tuitt.com>
2025-10-02 18:32:40 +0800

    Added files for pushing

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card col-md-3">
+        <!-- Use ":" directive to bind the project.image to the src attribute -->
+        <img :src="project.image" class="card-img-top card-img" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p class="card-text">
+            {{ project.description }}
+        </p>        
+        </div>
+    </div>
+</template>
+
+<script setup> 
+import { defineProps } from 'vue';
+
+defineProps({
+    project: Object
+})
+
+</script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,44 @@
+<template>
+	<!-- My Projects -->
+	<section class="pb-5" id="projects">
+
+        <h1 class="mt-5 mb-5 pb-4 text-center">My Projects</h1>
+
+        <!-- Loop through each group of 3 projects -->
+
+        <div
+            v-for="(group, index) in chunkedProjects"
+            :key="index"
+            class="card-deck my-5 justify-content-center"
+        >
+            <!-- For each project in the current group, render a project card componet -->
+        <ProjectCard
+            v-for="project in group"
+            :key="project.id"
+            :project="project"
+        />  
+        </div>
+    </section>
+
+
+</template>
+
+<script setup>
+    import { computed } from 'vue';
+
+    import ProjectCard from './ProjectCard.vue';
+
+    import projects from '../data/projects.json';
+
+    const chunkSize = 3
+
+    const chunkedProjects = computed(() => {
+        const chunks = []; //Start with an empty list of rows
+
+        for (let i = 0; i < projects.length; i += chunkSize) {
+            chunks.push(projects.slice(i, i + chunkSize))
+        }
+
+        return chunks;
+    })
+</script>
